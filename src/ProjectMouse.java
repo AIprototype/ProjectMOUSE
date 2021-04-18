b2b7a7316835e73eb0690e5f029fc7b6c0a141ef
vinup <vinupolly@gmail.com>
2021-04-18 16:51:37 +0100

    Initial Commit

---
+++ src/ProjectMouse.java
@@ -0,0 +1,10 @@
+import processing.core.PApplet;
+
+public class ProjectMouse extends PApplet {
+
+    public static void main(String[] args){
+        PApplet.main("ProjectMouse");
+        System.out.println("Welcome to Project MOUSE !!");
+    }
+
+}
