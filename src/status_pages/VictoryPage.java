28863b9f5eaf01753ee4ea0766f6964e4fa4510d
Vinu Polly <vinupolly@gmail.com>
2021-05-18 21:49:46 +0100

    Added a victory.png for VictoryPage.java

--- src/status_pages/VictoryPage.java
+++ src/status_pages/VictoryPage.java
@@ -1,6 +1,7 @@
 package status_pages;
 
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class VictoryPage {
     String victoryTitle;
@@ -11,6 +12,7 @@ public class VictoryPage {
     float totalScoreValueWidth, totalScoreValueHeight, totalScoreValueTextSize;
     PApplet pApplet;
     float x, y;
+    PImage victoryLogo;
 
     public VictoryPage(PApplet pApplet, String victoryTitle, String totalScoreText) {
         this.pApplet = pApplet;
@@ -18,9 +20,11 @@ public class VictoryPage {
         this.totalScoreText = totalScoreText;
         this.victoryTitleTextSize = 40;
         this.totalScoreTextTextSize = 20;
-        this.totalScoreValueTextSize = 30;
+        this.totalScoreValueTextSize = 35;
         this.x = pApplet.width / 2;
         this.y = pApplet.height / 2;
+        this.victoryLogo = pApplet.loadImage("victory.png");
+        victoryLogo.resize(600, 100);
 
         calculateStringParams();
     }
@@ -50,16 +54,14 @@ public class VictoryPage {
 
     public void display() {
         pApplet.background(0);
-        pApplet.textSize(victoryTitleTextSize);
-        pApplet.fill(255, 255, 255);
-        pApplet.text(victoryTitle, x - victoryTitleWidth/2, y);
+        pApplet.image(victoryLogo, x - victoryLogo.width/2, y - victoryLogo.height);
 
         pApplet.textSize(totalScoreTextTextSize);
         pApplet.fill(255, 255, 255);
         pApplet.text(totalScoreText, x - totalScoreTextWidth/2, y + (victoryTitleHeight + totalScoreTextHeight));
 
         pApplet.textSize(totalScoreValueTextSize);
-        pApplet.fill(255, 255, 255);
+        pApplet.fill(255, 255, 0);
         pApplet.text(totalScoreValue, x - totalScoreValueWidth/2, y + (victoryTitleHeight + totalScoreTextHeight + totalScoreValueHeight));
     }
 }
